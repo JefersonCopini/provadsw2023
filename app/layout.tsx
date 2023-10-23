3e8458a2ce812a855c06a4b56276c7c25e98ee0e
JefersonCopini <116360705+JefersonCopini@users.noreply.github.com>
2023-10-23 23:54:01 +0000

    Fix:Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    (
+      <html lang="en">
+      <body className="max-w-[1440px] m-auto bg-[#3d3d3d]">{children}</body>
     </html>
+    )
   )
 }
