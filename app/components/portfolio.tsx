194cd1d325ff4e67624783317149e238d455679e
JefersonCopini <116360705+JefersonCopini@users.noreply.github.com>
2023-10-24 00:46:40 +0000

    fix:portfolio, imgs

---
+++ app/components/portfolio.tsx
@@ -0,0 +1,84 @@
+export default function portfolio() {
+    return (
+        <main className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <div className="md:col-span-2 lg:col-span-3 mt-4 text-white text-center">
+                <h2 id="portfolio">
+                    My portfolio <span>PORTFOLIO</span>
+                </h2>
+            </div>
+            <div className="bg-[#4d4d4d] rounded-md pb-2">
+                <a href="/curso_html.html">
+                    <img className="hover:scale-105" src="/img/pic01.jpg" alt="" />
+                    <div className="text-white text-center">
+    
+                    </div>
+                </a>
+            </div>
+            <div className="bg-[#4d4d4d] rounded-md pb-2">
+                <a href="/curso_css.html">
+                    <img src="/img/pic.jpg" alt="" />
+                    <div className="text-white text-center">
+                    </div>
+                </a>
+            </div>
+            <div className="bg-[#4d4d4d] rounded-md pb-2">
+                <a href="/curso_js.html">
+                    <img src="/img/pic03.jpg" alt="" />
+                    <div className="text-white text-center">
+                        
+                    </div>
+                </a>
+            </div>
+            <div className="bg-[#4d4d4d] rounded-md pb-2">
+                <a href="/curso_html.html">
+                    <img src="/img/pic04,jpg" alt="" />
+                    <div className="text-white text-center">
+                        <h3>Curso de DESIGN</h3>
+                        <p>O curso é realizado por profissionais..</p>
+                    </div>
+                </a>
+            </div>
+            <div className="bg-[#4d4d4d] rounded-md pb-2">
+                <a href="/curso_html.html">
+                    <img src="/img/pic05.jpg" alt="" />
+                    <div className="text-white text-center">
+                       
+                    </div>
+                </a>
+            </div>
+            <div className="bg-[#4d4d4d] rounded-md pb-2">
+                <a href="/curso_html.html">
+                    <img src="/img/pic06.jpg" alt="" />
+                    <div className="text-white text-center">
+                    
+                    </div>
+                </a>
+            </div>
+            <div className="bg-[#4d4d4d] rounded-md pb-2">
+                <a href="/curso_html.html">
+                    <img src="/img/pic07.jpg" alt="" />
+                    <div className="text-white text-center">
+                    
+                    </div>
+                </a>
+            </div>
+            <div className="bg-[#4d4d4d] rounded-md pb-2">
+                <a href="/curso_html.html">
+                    <img src="/img/pic08.jpg" alt="" />
+                    <div className="text-white text-center">
+                    
+                    </div>
+                </a>
+            </div>
+            <div className="bg-[#4d4d4d] rounded-md pb-2">
+                <a href="/curso_html.html">
+                    <img src="/img/pic09.jpg" alt="" />
+                    <div className="text-white text-center">
+                    
+                    </div>
+                </a>
+            </div>
+        </main>
+
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Footer from './components/footer'
 import Banner from './components/banner'
+import Portfolio from './components/portfolio'
 
 export default function Home() {
   return (
     <div>
       <header />
     <Banner />
+    <Portfolio />
     <Footer />
     </div>
   )
