fd65c91f9d0e8810163cd4f7d42b6f4ca021313c
JefersonCopini <116360705+JefersonCopini@users.noreply.github.com>
2023-10-24 02:05:42 +0000

    fix:portfolio

--- app/components/portfolio.tsx
+++ app/components/portfolio.tsx
@@ -1,61 +1,67 @@
-export default function portfolio() {
+export default function Portfolio() {
     return (
-        <main className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-            <div className="md:col-span-2 lg:col-span-3 mt-4 text-white text-center">
-                <h2 id="portfolio">
-                    My portfolio <span>PORTFOLIO</span>
-                </h2>
+      <main className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <div className="md:col-span-2 lg:col-span-3 mt-4 text-white text-center">
+          <h2 id="portfolio">
+            My portfolio <span>PORTFOLIO</span>
+          </h2>
+        </div>
+        <div className="bg-[#4d4d4d] rounded-md pb-2">
+          <a href="#" className="flex flex-col items-center"> {/* Adicione a classe flex e items-center */}
+            <img src="/img/pic02.jpg" alt="" />
+            <div className="text-white text-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque non magnam doloribus rem. Quas, provident? Labore, doloremque totam minima nisi, voluptatum eligendi, possimus eaque neque consectetur ipsum dolore dolorum hic.
+              </p>
+              <a href="#" className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 hover:underline">Procurar Mais</a>
             </div>
-            <div className="bg-[#4d4d4d] rounded-md pb-2">
-                <a href="#">
-                    <img src="/img/pic02.jpg" alt="" />
-                    <div className="text-white text-center flex:items-center">
-                    
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque non magnam doloribus rem. Quas, provident? Labore, doloremque totam minima nisi, voluptatum eligendi, possimus eaque neque consectetur ipsum dolore dolorum hic.</p>
-                        <a href="#" className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 hover:underline">Procurar Mais</a>
-                    </div>
-                </a>
+          </a>
+        </div>
+        <div className="bg-[#4d4d4d] rounded-md pb-2">
+          <a href="#" className="flex flex-col items-center"> {/* Adicione a classe flex e items-center */}
+            <img src="/img/pic03.jpg" alt="" />
+            <div className="text-white text-center">
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni autem repellendus cumque sequi, animi, ea ipsum aperiam repudiandae vero distinctio, corporis incidunt eum neque quos ipsa atque omnis esse. Consectetur.
+              </p>
+              <a href="#" className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 hover:underline">Procurar Mais</a>
             </div>
-            <div className="bg-[#4d4d4d] rounded-md pb-2">
-                <a href="#">
-                    <img src="/img/pic03.jpg" alt="" />
-                    <div className="text-white text-center">
-                    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni autem repellendus cumque sequi, animi, ea ipsum aperiam repudiandae vero distinctio, corporis incidunt eum neque quos ipsa atque omnis esse. Consectetur.</p>
-                    <a href="#" className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 hover:underline">Procurar Mais</a>
-                    </div>
-                </a>
+          </a>
+        </div>
+        <div className="bg-[#4d4d4d] rounded-md pb-2">
+          <a href="#" className="flex flex-col items-center"> {/* Adicione a classe flex e items-center */}
+            <img src="/img/pic04.jpg" alt="" />
+            <div className="text-white text-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nobis consectetur, maxime quos itaque exercitationem blanditiis adipisci! Illo, excepturi nemo impedit officiis consequatur doloremque ea porro, quidem at omnis harum!
+              </p>
+              <a href="#" className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 hover:underline">Procurar Mais</a>
             </div>
-            <div className="bg-[#4d4d4d] rounded-md pb-2">
-                <a href="#">
-                    <img src="/img/pic04.jpg" alt="" />
-                    <div className="text-white text-center">
-                       <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nobis consectetur, maxime quos itaque exercitationem blanditiis adipisci! Illo, excepturi nemo impedit officiis consequatur doloremque ea porro, quidem at omnis harum!</p> 
-                       <a href="#" className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 hover:underline">Procurar Mais</a>
-                    </div>
-                </a>
+          </a>
+        </div>
+        <div className="bg-[#4d4d4d] rounded-md pb-2">
+          <a href="#" className="flex flex-col items-center"> {/* Adicione a classe flex e items-center */}
+          <img src="/img/pic06.jpg" alt="" />
+            <div className="text-white text-center">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus rem vero repudiandae expedita, laboriosam, perspiciatis pariatur autem nemo magnam facere, quidem vel officia! Pariatur molestiae quod esse nam excepturi!
+              </p>
+              <a href="#" className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 hover:underline">Procurar Mais</a>
             </div>
-           
-            <div className="bg-[#4d4d4d] rounded-md pb-2">
-                <a href="#">
-                    <img src="/img/pic06.jpg" alt="" />
-                    <div className="text-white text-center">
-                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ducimus rem vero repudiandae expedita, laboriosam, perspiciatis pariatur autem nemo magnam facere, quidem vel officia! Pariatur molestiae quod esse nam excepturi!</p>
-                       <a href="#" className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 hover:underline">Procurar Mais</a>
-                    </div>
-                </a>
+          </a>
+        </div>
+        <div className="bg-[#4d4d4d] rounded-md pb-2">
+          <a href="#" className="flex flex-col items-center"> {/* Adicione a classe flex e items-center */}
+            <img src="/img/pic07.jpg" alt="" />
+            <div className="text-white text-center">
+              <p>
+                Iure quibusdam, accusamus repudiandae, tenetur, corporis voluptas nostrum magni veritatis exercitationem ex nam recusandae minima neque ut quidem! Suscipit magni corrupti dolor.
+              </p>
+              <a href="#" className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 hover:underline">Procurar Mais</a>
             </div>
-            <div className="bg-[#4d4d4d] rounded-md pb-2">
-                <a href="#">
-                    <img src="/img/pic07.jpg" alt="" />
-                    <div className="text-white text-center">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quibusdam, accusamus repudiandae, tenetur, corporis voluptas nostrum magni veritatis exercitationem ex nam recusandae minima neque ut quidem! Suscipit magni corrupti dolor.</p>
-                    <a href="#" className="text-blue-500 border border-blue-500 rounded-md px-2 py-1 hover:underline">Procurar Mais</a>
-            
-                    </div>
-                </a>
-            </div>
-           
-        </main>
-
-    )
-}
+          </a>
+        </div>
+      </main>
+    );
+  }
+  
